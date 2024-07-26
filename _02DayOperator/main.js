bf71cfc061a313dc6f9007df2d47f3256197e072
suraj sonwane <suraj.sonwane1207@gmail.com>
2024-07-26 22:18:38 +0530

    _2Completed

---
+++ _02DayOperator/main.js
@@ -0,0 +1,46 @@
+// Task / Activities
+// Activity 1: Arithimetic Operations
+  // o Task 1: Write a programm to add numbers and log the result to the console.
+  console.log(100+100)
+  // o Task 2: Write a programm to substract numbers and log the result to the console.
+  console.log(200-100)
+    // o Task 3: Write a programm to multiply numbers and log the result to the console.
+    console.log(100*100)
+      // o Task 4: Write a programm to divide numbers and log the result to the console.
+  console.log(100/1)
+  // o Task 5: Write a programm to add remainder and log the result to the console.
+  console.log(100%2)
+  let num = 100
+
+  // Activity 2: Asignment Operators
+  // o Task 6:Use the += operator to add  to a variable and log the result to the console 
+  console.log(num += 100)
+  // o Task 7:Use the += operator to substract to a variable and log the result to the console
+  console.log(num -= 100)
+ // Activity 3: Comparison Operators
+  // o Task 8:Write a programm to compare two numbers using > and < and log the result console.
+console.log(10<9)
+console.log(10>9)
+
+  // o Task 9:Write a programm to compare two numbers using >= and <= and log the result console.
+  console.log(10<=9)
+  console.log(10>=9)
+  
+  // o Task 10:Write a programm to compare two numbers using == and === and log the result console.
+  console.log(10==9)
+  console.log(10===9)
+  
+  // Activity 4: Logical Operators
+  // o Task 11:Write a programm that uses the && operato to combine two conditions and log the result to the console.
+console.log(10&&9)
+
+  // o Task 12:Write a programm  that uses the && operato to combine two conditions and log the result to the console.
+  console.log(10||9)
+  
+  
+  // o Task 13:Write a programm  that uses the && operato to combine two conditions and log the result to the console.
+  console.log(!9)
+
+  // Activity5: ternary Operators
+  // o Task 1:Write a programm that uses the ternary operator to check if a number is positive or negative.
+console.log(10>0?"positive":"negative")
